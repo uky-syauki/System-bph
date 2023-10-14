731ca881deb3805fdcb399b9c02197a798244932
uky-syauki <achmad.uky@gmail.com>
2023-10-14 20:19:42 +0800

    7 local

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,9 @@ import base64
 import os
 
 app = Flask(__name__)
-# CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app)
-cors = CORS(app, resources={r"/socket.io/*": {"origins": "*"}})
+# cors = CORS(app, resources={r"/socket.io/*": {"origins": "*"}})
 
 @app.route('/')
 def index():
@@ -19,7 +19,7 @@ def index():
 
 @app.route('/camera')
 def camera():
-	return render_template('tsocket2.html')
+	return render_template('tsocket.html')
 
 
 @app.route('/api/get-data', methods=['GET','POST'])
