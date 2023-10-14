465a1fea9e57c82c85eccc59ec825c953b845178
uky-syauki <achmad.uky@gmail.com>
2023-10-14 21:28:34 +0800

    9 local

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 import cv2 as cv
 import numpy as np
@@ -8,9 +11,17 @@ import base64
 import os
 
 app = Flask(__name__)
+app.config.from_object(Config)
 CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 # cors = CORS(app, resources={r"/socket.io/*": {"origins": "*"}})
+
+
+# handle API
+
+
 
 @app.route('/')
 def index():
@@ -36,7 +47,7 @@ def get_data():
 def handle_message(message):
 	print("Menerima Pesan", message)
 	socketio.emit('message',message)
-	
+
 
 @socketio.on('daftar')
 def daftar(gambar):
