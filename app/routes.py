1e1b67578feed23461178c13563ee83406074683
uky-syauki <achmad.uky@gmail.com>
2023-10-14 22:48:07 +0800

    11 local

---
+++ app/routes.py
@@ -0,0 +1,24 @@
+from app import app, db
+from flask import request, redirect, jsonify
+from app.models import Calgot
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Bismillah"
+
+
+@app.route('/api/mendaftar', methods=['POST'])
+def mendaftar():
+    data = request.form
+    print("Menerima", data)
+    calgot = Calgot(nama_panggilan=data['nama_panggilan'],nama_lengkap=data['nama_lengkap'], email=data['email'],nomor_wa=data['nomorWA'], asal_kampus=data['asalKampus'])
+    db.session.add(calgot)
+    db.session.commit()
+    print("Calgot berhasil di daftar", data['nama_panggilan'])
+
+
+
+    # return redirect('https://google.com')
+    return jsonify({'status':'Sukses', 'pesan':'Data Diterima'})
+
