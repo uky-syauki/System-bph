b28eed3718d7b02ef41187bbd242147bf32979e9
uky-syauki <achmad.uky@gmail.com>
2023-10-14 21:46:05 +0800

    10 local

--- app/__init__.py
+++ app/__init__.py
@@ -19,44 +19,4 @@ migrate = Migrate(app, db)
 # cors = CORS(app, resources={r"/socket.io/*": {"origins": "*"}})
 
 
-# handle API
-
-
-
-@app.route('/')
-def index():
-	return render_template('base.html')
-
-
-@app.route('/camera')
-def camera():
-	return render_template('tsocket.html')
-
-
-@app.route('/api/get-data', methods=['GET','POST'])
-def get_data():
-	return jsonify(
-		{
-			'nama':'ahmad syauki',
-			'email':'achmad.uky@gmail.com'
-		}
-		)
-
-
-@socketio.on('pesan')
-def handle_message(message):
-	print("Menerima Pesan", message)
-	socketio.emit('message',message)
-
-
-@socketio.on('daftar')
-def daftar(gambar):
-	print('gambar di terima')
-	img = gambar.split(',')[1]
-	img_bin = img.encode('utf-8')
-	img_np = np.frombuffer(base64.b64decode(img_bin), dtype=np.uint8)
-	img = cv.imdecode(img_np, cv.IMREAD_COLOR)
-	if not os.path.exists('gambar'):
-		os.mkdir('gambar')
-	cv.imwrite('gambar/t2.png',img)
-	print('add gambar')
+from app import routes, models
